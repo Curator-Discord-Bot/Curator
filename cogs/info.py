ea933102a45bf465a3386ca89bd98e7915bceb6f
Robin Timmerman <robintimmerman@hotmail.nl>
2020-03-01 20:45:28 +0100

    Auto stash before merge of "master" and "origin/master"

--- cogs/info.py
+++ cogs/info.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import matplotlib.pyplot as plt
 
 
 class Info(commands.Cog):
@@ -10,6 +11,36 @@ class Info(commands.Cog):
     async def roles(self, ctx: commands.Context):
         roles = [f'{role.name}: {len(role.members)}' for role in await ctx.guild.fetch_roles() if role.name != '@everyone']
         await ctx.send('\n'.join(roles))
+
+    @commands.command()
+    async def pie(self, ctx: commands.context):
+        roles = [role for role in await ctx.guild.fetch_roles() if role.name != '@everyone']
+        members = []
+        c = {
+            'type': 'pie',
+            'data': {
+                'labels': [role.name for role in roles],
+                'datasets': [{
+                    'data': []
+                }]
+            }
+        }
+        for role in roles:
+            count = 0
+            for member in role.members:
+                if member.id in members:
+                    continue
+                members.append(member.id)
+                count += 1
+            c['data']['datasets'][0]['data'].append(count)
+        print(c)
+        plt.figure()
+        labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+        sizes = [15, 30, 45, 10]
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+        ax1.axis('equal')
+        plt.show()
 
 
 def setup(bot: commands.Bot):
