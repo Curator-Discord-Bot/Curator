1d06a08836671008bc30a795b5d61e9443912b86
Lucas Olai Jarlkov Olsen <lucas-olai@hotmail.com>
2020-03-01 19:10:04 +0100

    Auth test

--- cogs/mcchat.py
+++ cogs/mcchat.py
@@ -1,42 +1,58 @@
+import asyncpg
 import discord
+import asyncio
 from discord.ext import commands
 
-from twisted.internet import reactor
+from twisted.internet import asyncioreactor, reactor
 from quarry.net.server import ServerFactory, ServerProtocol
 
 
-class DowntimeProtocol(ServerProtocol):
-    def packet_login_start(self, buff):
-        buff.discard()
-        self.close(self.factory.motd)
+class AuthProtocol(ServerProtocol):
+    def player_joined(self):
+        # This method gets called when a player successfully joins the server.
+        #   If we're in online mode (the default), this means auth with the
+        #   session server was successful and the user definitely owns the
+        #   display name they claim to.
+
+        # Call super. This switches us to "play" mode, marks the player as
+        #   in-game, and does some logging.
+        ServerProtocol.player_joined(self)
+
+        # Define your own logic here. It could be an HTTP request to an API,
+        #   or perhaps an update to a database table.
+        display_name = self.display_name
+        ip_addr = self.remote_addr.host
+        self.logger.info("[%s authed with IP %s]" % (display_name, ip_addr))
+
+        # Kick the player.
+        self.close("Thanks, you are now registered!")
 
 
-class DowntimeFactory(ServerFactory):
-    protocol = DowntimeProtocol
+class AuthFactory(ServerFactory):
+    protocol = AuthProtocol
+    motd = "Auth Server"
 
 
-def run():
-    try:
-        # Create factory
-        factory = DowntimeFactory()
-        factory.motd = 'Hello'
+def main(argv):
+    # Parse options
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--host", default="", help="address to listen on")
+    parser.add_argument("-p", "--port", default=25565, type=int, help="port to listen on")
+    args = parser.parse_args(argv)
 
-        # Listen
-        factory.listen("", 25565)
-        reactor.run()
-    except Exception as e:
-        print('Server run failed:', e)
+    # Create factory
+    factory = AuthFactory()
 
-
-class MCChat(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-
-    @commands.command()
-    async def run(self, ctx: commands.Context):
-        await ctx.send('Let\'s try this!')
-        run()
+    # Listen
+    factory.listen(args.host, args.port)
+    asyncioreactor.run()
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(MCChat(bot))
+    asyncioreactor.install(asyncio.get_event_loop())
+    main([])
+
+
+def teardown(bot):
+    asyncioreactor.stop()
