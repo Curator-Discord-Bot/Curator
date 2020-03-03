1baca40d448cdad64b298b21404c09b46bfe15ce
Robin Timmerman <robintimmerman@hotmail.nl>
2020-03-03 21:26:40 +0100

    pie iie

--- cogs/info.py
+++ cogs/info.py
@@ -14,23 +14,42 @@ class Info(commands.Cog):
         await ctx.send('\n'.join(roles))
 
     @commands.command()
-    async def pie(self, ctx: commands.context):
-        roles = [role for role in await ctx.guild.fetch_roles() if role.name != '@everyone']
-        labels = []
-        members = []
-        sizes = []
-        colors = []
-        for role in roles:
-            count = 0
-            for member in role.members:
-                if member.id in members:
-                    continue
-                members.append(member.id)
-                count += 1
-            if count > 0:
-                labels.append(role.name)
-                sizes.append(count)
-                colors.append(str(role.color))
+    async def pie(self, ctx: commands.context, infinity='no', traveller='no'):
+        if infinity == 'iie':
+            if ctx.guild.id == '468366604313559040':
+                labels = ['Traveler', 'Citizen', 'Squire', 'Knight', "Lord", 'Hero', 'Legend']
+                sizes = []
+                colors = []
+                members = []
+                for role in labels:
+                    count = 0
+                    for member in role.members:
+                        if member.id in members:
+                            continue
+                        members.append(member.id)
+                        count += 1
+                    sizes.append(count)
+                    colors.append(str(role.color))
+            else:
+                ctx.send('This isn\'t the official Inifnity Item Editor Discord server.')
+        #   I could also combine this if and else to get less code lines, it will take more computing power tho
+        else:
+            roles = [role for role in await ctx.guild.fetch_roles() if role.name != '@everyone']
+            labels = []
+            sizes = []
+            colors = []
+            members = []
+            for role in roles:
+                count = 0
+                for member in role.members:
+                    if member.id in members:
+                        continue
+                    members.append(member.id)
+                    count += 1
+                if count > 0:
+                    labels.append(role.name)
+                    sizes.append(count)
+                    colors.append(str(role.color))
         plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%')
         plt.axis('equal')
         buf = BytesIO()
