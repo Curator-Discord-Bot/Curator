fc299dfdd8a2ca47311bbf05a7d2da94cfe7c79c
Robin Timmerman <robintimmerman@hotmail.nl>
2020-04-03 14:42:25 +0200

    ,count check now mentions the active count channel(s)
    forgot I wanted to do this before I sent my previous commit

--- cogs/count.py
+++ cogs/count.py
@@ -327,9 +327,8 @@ class Count(commands.Cog):
         channels = ctx.guild.text_channels
         for channel in channels:
             if channel.id in running_counts.keys():
-                await ctx.send('A count is running.')
-                return
-        await ctx.send('No count is running.')
+                await ctx.send(f'A count is running in {ctx.channel.mention}.')
+        await ctx.send('No count is running on this server.')
         return
 
     @count.command()
