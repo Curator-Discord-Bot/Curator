268af5585c04a9613213ae1a2769ef972292dc2b
Lucas <lucas-olai@hotmail.com>
2020-07-30 15:10:31 +0200

    Update cog template

--- cogs/template.py
+++ cogs/template.py
@@ -1,14 +1,22 @@
 import discord
 from discord.ext import commands
+from asyncio import sleep as slp
 
 
 class Template(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command()
-    async def hello(self, ctx: commands.Context):
-        await ctx.send('Hello, ' + ctx.author.mention + '!')
+    @commands.group(invoke_without_command=True, name='template')
+    async def base(self, ctx: commands.Context):
+        """The base command for _"""
+        await ctx.send(f'This is the base command. See `{ctx.prefix}help base` for help on other commands.')
+
+    @base.command(name='template')
+    async def echo(self, ctx: commands.Context, *, msg: str):
+        """A template command."""
+        await slp(1)
+        await ctx.send(msg)
 
 
 def setup(bot: commands.Bot):
