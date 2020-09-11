ef8fb69c9024a3ac375cab43acf4ff7cf9961554
Lucas <lucas-olai@hotmail.com>
2020-09-11 12:36:49 +0200

    Kalah

---
+++ cogs/kalah.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+
+class Kalaha():
+
+
+
+class Kalah(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.group(invoke_without_command=True, aliases=['kalaha', 'mancala'])
+    async def kalah(self, ctx: commands.Context):
+        """The base command for kalah games"""
+        await ctx.send(f'This is the base command. See `{ctx.prefix}help kalah` for help on other commands.')
+
+    @kalah.command()
+    async def start(self, ctx: commands.Context, *, msg: str):
+        """A template command."""
+        await ctx.send(msg)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Kalah(bot))
